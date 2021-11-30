936bf97d52ffb361a46b807e9adcf4f15a7c505b
valqua2007 <quanduongforworks2007@gmail.com>
2021-11-30 15:02:18 +0700

    setup unit test with ts-jest

---
+++ src/lectures/04-main.test.ts
@@ -0,0 +1,14 @@
+import { isPositive } from './04-main';
+describe('isPositive()', () => {
+  it(`Should return true when n > 0`, () => {
+    expect(isPositive(1)).toBe(true);
+    expect(isPositive(2)).toBe(true);
+    expect(isPositive(3)).toBe(true);
+  });
+  it(`Should return false when n < 0`, () => {
+    expect(isPositive(-1)).toBe(false);
+  });
+  it(`Should return false when n = 0`, () => {
+    expect(isPositive(0)).toBe(false);
+  });
+});
